72420e10b4a898fdce054369deae9edd860125a2
Riley Mathews <rileymathews80@gmail.com>
2018-07-05 14:53:21 -0500

    started projct

---
+++ stocks.py
@@ -0,0 +1,17 @@
+stock_dict = {
+    "GE": "Galactic Empire",
+    "RA": "Rebel Alliance",
+    "TF": "Trade Federation"
+}
+
+purchases = [
+    ("GE", 50, '10-sep-2001', 24),
+    ("TF", 200, '10-jan-2002', 5),
+    ("RA", 20, '14-jun-2004', 50),
+    ("GE", 50, '15-dec-2004', 10)
+]
+
+print(stock_dict)
+print(purchases)
+
+
